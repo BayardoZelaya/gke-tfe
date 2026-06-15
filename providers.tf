16659b28ff69c175b8b38a2405df2d5b2a47ec97
Bayardo Zelaya <bayardozelaya@gmail.com>
2026-06-14 23:49:16 -0400

    fix fmt

--- providers.tf
+++ providers.tf
@@ -6,15 +6,15 @@ provider "google" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host  = "https://${module.gke.kubernetes_cluster_host}"
-  token = data.google_client_config.default.access_token
+  host                   = "https://${module.gke.kubernetes_cluster_host}"
+  token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes = {
-    host  = "https://${module.gke.kubernetes_cluster_host}"
-    token = data.google_client_config.default.access_token
+    host                   = "https://${module.gke.kubernetes_cluster_host}"
+    token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
   }
 }
